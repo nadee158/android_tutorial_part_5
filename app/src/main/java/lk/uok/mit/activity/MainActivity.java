b1fd7189cbe667c286b5932ef971171f2ab271bd
Nadeeshani Senevirathna <nadee158@gmail.com>
2018-06-20 12:19:34 +0530

    modified

---
+++ app/src/main/java/lk/uok/mit/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package lk.uok.mit.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lk.uok.mit.helloworld.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
